a26b619be88d6b999be0d6e14f9f9f4233548877
Lagatphineas <lagatphineas@gmail.com>
2025-09-21 12:43:14 +0300

    Add files via upload

---
+++ c program assignment 1_123006.cpp
@@ -0,0 +1,19 @@
+/*
+Name:Phineas Kiprotich
+REG NO:PA106/G/29213/25
+Description:A simple program to display all the units for the semester
+*/
+
+#include<stdio.h>//preprocessor directive
+//main function
+int main(){ 
+	printf("1.Mathematics for science \n");
+	printf("2.Discrete mathematics \n");
+	printf("3.Communication skills \n");
+	printf("4.Introduction to programmimg and problem solving \n");
+	printf("5.Fundamentals of ICT \n");
+	printf("6.Computer systems and organization \n");
+	printf("7.Fundamentals of system analysis and design \n");
+	
+	return 0;
+}
